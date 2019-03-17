94bf6dccb23d222c51fd242d71273a8debf127e5
magras <magras@users.noreply.github.com>
2019-03-17 19:10:49 +0300

    refactor TaskManager

--- shared/utils/TaskManager.cpp
+++ shared/utils/TaskManager.cpp
@@ -1,42 +1,31 @@
-#include <TaskManager.h>
-
-TaskManager::TaskManager()
-{
-}
-
-TaskManager::~TaskManager()
-{
-	for (Task* task : tasks)
-	{
-		delete task;
-	}
-}
+#include "TaskManager.h"
+#include "Global.h"
 
 void TaskManager::push(std::unique_ptr<Task> task)
 {
 	S::log.add("Task [" + task->getName() + "] is added", { LOG_TAGS::TASK });
-	tasks.push_back(task.release());
+	tasks.push_back(std::move(task));
 }
 
 void TaskManager::update()
 {
-	for (Task* &task : tasks)
+	// TODO: Merge this loop with the next one?
+	//       Or at least explain why it has been separated.
+	for (auto && task : tasks)
 	{
 		task->update();
 	}
 
-	for (Task* &task : tasks)
+	for (auto && task : tasks)
 	{
 		if (task->isComplete())
 		{
 			S::log.add("Task [" + task->getName() + "] is complete", { LOG_TAGS::TASK });
-			auto i = &task - &tasks[0];
-			delete task;
-			tasks[i] = nullptr;
+			task = nullptr;
 		}
 	}
 
-	tasks.erase(std::remove_if(tasks.begin(), tasks.end(), [](Task* task) {
+	tasks.erase(std::remove_if(tasks.begin(), tasks.end(), [](const auto& task) {
 		return task == nullptr;
 	}), tasks.end());
 }
