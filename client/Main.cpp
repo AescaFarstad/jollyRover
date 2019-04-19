3b359fc2998b5a0ac55b5f02fcd099950d35dcd5
magras <magras@users.noreply.github.com>
2019-04-19 19:08:59 +0300

    get rid of old plain C style

--- client/Main.cpp
+++ client/Main.cpp
@@ -1,8 +1,8 @@
-
 #include <Game.h>
 #include <SystemInfo.h>
 #include <FPSMeter.h>
 #include <SDL_gpu.h>
+#include <memory>
 
 //#define WINDOWS 1
 #define LINUX 1
@@ -37,7 +37,7 @@ namespace MainInternal
 
 using namespace MainInternal;
 
-void mainLoop(void* arg)
+void mainLoop(void*)
 {
 	SDL_Event e;
 	while (SDL_PollEvent(&e) != 0)
@@ -138,7 +138,7 @@ typedef struct EmscriptenWebGLContextAttributes {
 	if (screen == NULL)
 	{
 		printf("Failed to create window %s\n", SDL_GetError());
-		return 0;
+		return 1;
 	}
 	
 	game = new Game(screen);
@@ -169,40 +169,33 @@ typedef struct EmscriptenWebGLContextAttributes {
 
 void printRenderers()
 {
-	SDL_version compiled;
-	SDL_version linked;
-	GPU_RendererID* renderers;
-	int i;
-	int order_size;
-	GPU_RendererID order[GPU_RENDERER_ORDER_MAX];
-
     GPU_SetDebugLevel(GPU_DEBUG_LEVEL_MAX);
     
-    compiled = GPU_GetCompiledVersion();
-    linked = GPU_GetLinkedVersion();
+    SDL_version compiled = GPU_GetCompiledVersion();
+    SDL_version linked = GPU_GetLinkedVersion();
     if(compiled.major != linked.major || compiled.minor != linked.minor || compiled.patch != linked.patch)
         GPU_Log("SDL_gpu v%d.%d.%d (compiled with v%d.%d.%d)\n", linked.major, linked.minor, linked.patch, compiled.major, compiled.minor, compiled.patch);
     else
         GPU_Log("SDL_gpu v%d.%d.%d\n", linked.major, linked.minor, linked.patch);
     
-	renderers = (GPU_RendererID*)malloc(sizeof(GPU_RendererID)*GPU_GetNumRegisteredRenderers());
-	GPU_GetRegisteredRendererList(renderers);
-	
+	auto renderers = std::make_unique<GPU_RendererID[]>(GPU_GetNumRegisteredRenderers());
+	GPU_GetRegisteredRendererList(renderers.get());
 	GPU_Log("\nAvailable renderers:\n");
-	for(i = 0; i < GPU_GetNumRegisteredRenderers(); i++)
+	for(int i = 0; i < GPU_GetNumRegisteredRenderers(); i++)
 	{
 		GPU_Log("* %s (%d.%d)\n", renderers[i].name, renderers[i].major_version, renderers[i].minor_version);
 	}
-	GPU_Log("Renderer init order:\n");
-	
+
+	GPU_RendererID order[GPU_RENDERER_ORDER_MAX];
+	int order_size;
 	GPU_GetRendererOrder(&order_size, order);
-	for(i = 0; i < order_size; i++)
+	GPU_Log("Renderer init order:\n");
+	for(int i = 0; i < order_size; i++)
 	{
 		GPU_Log("%d) %s (%d.%d)\n", i+1, order[i].name, order[i].major_version, order[i].minor_version);
 	}
-	GPU_Log("\n");
 
-	free(renderers);
+	GPU_Log("\n");
 }
 
 void printCurrentRenderer()
